75c9358f1dc6b42c6af3423ba396ce0af049886a
bmotevalli <b.motevalli@gmail.com>
2025-03-06 22:34:01 +0800

    storing tasks in local storage.

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,10 +1,16 @@
 import { Injectable, signal } from '@angular/core';
-import { DUMMY_TASKS } from '../dummy-tasks';
 import { Task } from './task/task.model';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
-  private tasks = signal(DUMMY_TASKS);
+  private tasks = signal<Task[]>([]);
+
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks.set(JSON.parse(tasks));
+    }
+  }
 
   getUserTasks(userId: string | undefined) {
     if (userId) {
@@ -16,20 +22,25 @@ export class TasksService {
   deleteTask(taskId: string | undefined) {
     if (taskId) {
       this.tasks.set(this.tasks().filter((r) => r.id !== taskId));
+      this.saveTasks();
     }
   }
 
   addNewTask(task: Task, userId: string | undefined) {
-    if (!userId) {
-      return;
+    if (userId) {
+      this.tasks.set([
+        {
+          ...task,
+          userId: userId,
+          id: new Date().toDateString(),
+        },
+        ...this.tasks(),
+      ]);
+      this.saveTasks();
     }
-    this.tasks.set([
-      {
-        ...task,
-        userId: userId,
-        id: new Date().toDateString(),
-      },
-      ...this.tasks(),
-    ]);
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks()));
   }
 }
